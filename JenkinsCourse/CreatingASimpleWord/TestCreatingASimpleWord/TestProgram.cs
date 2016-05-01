18c3e4e23773e7be10074b690280e4b61b33de4a
codrutas <codrutas@yahoo.com>
2016-05-01 18:19:10 +0300

    Test Project for Jenkins course

---
+++ JenkinsCourse/CreatingASimpleWord/TestCreatingASimpleWord/TestProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CreatingASimpleWord;
+
+namespace TestCreatingASimpleWord
+{
+    [TestFixture]
+    class TestProgram
+    {
+       [Test]
+        public void ValidateMessage()
+       {
+           Assert.AreEqual("Hello World!", Program.CreateMessage(), "Mesaj incorect");
+       }
+    }
+}
